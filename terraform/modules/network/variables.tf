7892233337415abc71631adc88e4e7707a72869b
Johannes Reichhardt <johannes.reichhardt@gmail.com>
2026-03-31 19:18:55 -0500

    Fix: Correct HCL syntax in module variables

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -1,3 +1,11 @@
-variable "project_id" { type = string }
-variable "region" { type = string }
-variable "env_name" { type = string }
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "env_name" {
+  type = string
+}
--- terraform/modules/secrets/variables.tf
+++ terraform/modules/secrets/variables.tf
@@ -1,3 +1,12 @@
-variable "project_id" { type = string }
-variable "gemini_api_key" { type = string; sensitive = true }
-variable "eso_gsa_email" { type = string }
+variable "project_id" {
+  type = string
+}
+
+variable "gemini_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "eso_gsa_email" {
+  type = string
+}

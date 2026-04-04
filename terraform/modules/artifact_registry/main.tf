7c132e1fc9d30ae0af28c7543c4dcf7b09c0d8ec
Johannes Reichhardt <johannes.reichhardt@gmail.com>
2026-04-04 15:52:45 -0500

    terraform: automate initial image build for txt2md

--- terraform/modules/artifact_registry/main.tf
+++ terraform/modules/artifact_registry/main.tf
@@ -7,3 +7,16 @@ resource "google_artifact_registry_repository" "docker_repo" {
     prevent_destroy = false
   }
 }
+
+# Automatisierter Initial-Build (Bootstrap)
+resource "null_resource" "bootstrap_image" {
+  triggers = {
+    repository_id = google_artifact_registry_repository.docker_repo.id
+  }
+
+  provisioner "local-exec" {
+    command = "gcloud builds submit https://github.com/joreichhardt/txt2md.git --tag ${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.docker_repo.repository_id}/txt2md:latest --project=${var.project_id}"
+  }
+
+  depends_on = [google_artifact_registry_repository.docker_repo]
+}

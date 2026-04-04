e0f38151a8ad76ce7bf583b9a8accc1ee6c29079
Johannes Reichhardt <johannes.reichhardt@gmail.com>
2026-04-04 18:00:59 -0500

    terraform: add get_credentials_command to outputs for easier cluster management

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,8 @@ output "artifact_registry_repo" {
 output "argocd_url" {
   value = "https://argocd.${var.domain_name}"
 }
+
+output "get_credentials_command" {
+  value       = "gcloud container clusters get-credentials ${module.gke.cluster_name} --region ${var.region} --project ${var.project_id}"
+  description = "Run this command to update your local kubeconfig"
+}
